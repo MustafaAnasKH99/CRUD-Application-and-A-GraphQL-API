076c94f004c8ef1b15f12683866546989c05c35e
Mustafa Anas <ad16mkha@uwcad.it>
2019-02-27 12:15:19 +0200

    Setup Online Mongo and Mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect("mongodb+srv://tester:tester123@cluster0-9sdzj.mongodb.net/test?retryWrites=true")
+mongoose.connection.once('open', () => {console.log('connected to DB')})
 
 app.use('/BeMyGuest', graphqlHTTP({
     schema,
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql')
 const _ = require('lodash')
+const Product = require('../models/Product')
+const Category = require('../models/Category')
 
 const {
     GraphQLObjectType,
@@ -9,22 +11,6 @@ const {
     GraphQLList
 } = graphql
 
-
-// the dummies
-const products = [
-    {id: "1", name: "batata", categoryId: "3"},
-    {id: "2", name: "pizza", categoryId: "3"},
-    {id: "3", name: "makdoos", categoryId: "3"}
-]
-// the dummies
-const categories = [
-    {id: "1", name: "yummy"},
-    {id: "2", name: "aargh"},
-    {id: "3", name: "meh"}
-]
-
-
-
 const ProductObjectType = new GraphQLObjectType({
     name: 'Product',
     fields: () => ({
@@ -33,7 +19,7 @@ const ProductObjectType = new GraphQLObjectType({
         category: {
             type: CategoryObjectType,
             resolve(parent, args){
-                return _.find(products, {id: parent.categoryId})
+                // return _.find(products, {id: parent.categoryId})
             }
         }
     })
@@ -47,7 +33,7 @@ const CategoryObjectType = new GraphQLObjectType({
         products: {
             type: new GraphQLList(ProductObjectType),
             resolve(parent, args){
-                return _.filter(products, {categoryId: parent.id})
+                // return _.filter(products, {categoryId: parent.id})
             }
         }, 
     })
@@ -61,7 +47,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args){
               // code to get data from db
-             return _.find(products, {id: args.id})
+            //  return _.find(products, {id: args.id})
             }
         },
 
@@ -70,7 +56,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args){
               // code to get data from db
-             return _.find(categories, {id: args.id})
+            //  return _.find(categories, {id: args.id})
 
             }
         },
@@ -78,7 +64,7 @@ const RootQuery = new GraphQLObjectType({
         categories: {
             type: new GraphQLList(CategoryObjectType),
             resolve(parent, args){
-                return categories
+                // return categories
             }
         }
     }
