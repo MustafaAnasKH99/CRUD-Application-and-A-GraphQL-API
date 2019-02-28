d073a60df44c2418f80e5d69292592cd5cf4e7b9
Mustafa Anas <ad16mkha@uwcad.it>
2019-02-28 17:37:40 +0200

    Add Some CDD

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
-const cors = require('cors') // allow requests from another server
+const cors = require('cors')
 
 const app = express()
 app.use(cors())
